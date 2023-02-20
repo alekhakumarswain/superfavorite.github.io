26188262e93ba2566728454406b00221c2f99047
Alekha Kumar Swain <51305914+alekhakumarswain@users.noreply.github.com>
2023-02-21 00:14:28 +0530

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAy51n8WrsAy8R-xbXNanqL9AHQFPtuiZY",
+  authDomain: "sufev7852.firebaseapp.com",
+  databaseURL: "https://sufev7852-default-rtdb.firebaseio.com",
+  projectId: "sufev7852",
+  storageBucket: "sufev7852.appspot.com",
+  messagingSenderId: "733657119988",
+  appId: "1:733657119988:web:7cfdf2d65de520ee21f2a1"		
+     
+};
+
+firebase.initializeApp(firebaseConfig);
